297ed8aa45aecf2f0f6a0e41f39df53657a1fd4d
Ilya Zaytsev <you@example.com>
2018-04-21 19:51:01 +0400

    IDwarfCrawler.h.

---
+++ IDwarfCrawler.h
@@ -0,0 +1,34 @@
+#include <string>
+#include <experimental/optional>
+
+namespace dwarf_crawler
+{
+
+enum class DwarfTag
+{
+    Unknown,
+    Namespace,
+    Class,
+    Struct,
+    Subroutine,
+};
+
+struct DwarfNode
+{
+    const DwarfTag Tag;
+    const std::string Name;
+};
+
+struct IDwarfCrawler
+{
+    template <typename T>
+    using TOptional = std::experimental::optional<T>;
+
+    virtual ~IDwarfCrawler() = default;
+
+    virtual TOptional<DwarfNode> NextSibling() = 0;
+
+    virtual TOptional<DwarfNode> NextChild() = 0;
+};
+
+}
